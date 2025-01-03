8bf780a50a7a27876bf1e266b637481ffc406cbb
CThomson2 <s2076431@ed.ac.uk>
2025-01-03 14:56:46 +0000

    Set up inventory Prisma schema

---
+++ app/(routes)/inventory/transactions/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+import React from "react";
+
+/*
+This client component will display the inventory transactions table,
+taking data from the database and displaying it with live updates.
+
+There will also be the ability to manage inventory transactions.
+This means adding records to the table - not modifying or deleting existing records.
+
+Adding a record involves an input form, which will confirm that all required fields are filled out, and will utilise type safety to ensure that the data is valid with the database constraints.
+*/
+const InventoryTable = () => {
+  return <div>Inventory Table</div>;
+};
+
+export default InventoryTable;
